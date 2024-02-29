56fbb758d6c58ea0ab7c1a5b4a651102fb218800
Dennis Adigwe <adigwedennis@gmail.com>
2024-02-29 01:38:47 +0100

    updated task 1 to return node created if parent is null

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -14,6 +14,8 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	binary_tree_t *node;
 
 	node = binary_tree_node(parent, value);
+	if (parent == NULL)
+		return node;
 	if (parent->left != NULL)
 	{
 		node->left = parent->left;
