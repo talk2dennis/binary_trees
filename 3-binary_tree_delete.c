eb778f8a67344eadc8122989c440e9fc9bf7dc1e
Dennis Adigwe <adigwedennis@gmail.com>
2024-02-29 01:07:31 +0100

    completed task 3

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - A function that deletes an entire binary tree
+ * @tree: is a pointer to the root node of the tree to delete
+ * Return: return nothing
+ */
+
+
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return;
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
+	free(tree);
+}
