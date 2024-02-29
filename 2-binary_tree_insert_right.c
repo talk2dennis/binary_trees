f967bc7d475edf9e994a433626e49c9953377ec1
Dennis Adigwe <adigwedennis@gmail.com>
2024-02-29 01:48:49 +0100

    added a check for null parent

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -13,6 +13,8 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *node;
 
+	if (parent == NULL)
+		return (NULL);
 	node = binary_tree_node(parent, value);
 	if (node == NULL)
 		return (NULL);
