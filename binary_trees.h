91b8c36963a6af61dd2cf4a1406b64b6d33afee4
Dennis Adigwe <adigwedennis@gmail.com>
2024-02-29 01:01:45 +0100

    created the header file and the first task

---
+++ binary_trees.h
@@ -0,0 +1,37 @@
+#ifndef BTREE_H
+#define BTREE_H
+
+#include <strings.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
+struct binary_tree_s
+{
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
+};
+
+typedef struct binary_tree_s binary_tree_t;
+
+/** tasks headers */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+void binary_tree_delete(binary_tree_t *tree);
+
+/** helper function headers */
+void binary_tree_print(const binary_tree_t *);
+
+#endif
