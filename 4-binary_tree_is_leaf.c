de08a3c2b2461c1547de64870455e51b157921b8
Dennis Adigwe <adigwedennis@gmail.com>
2024-02-29 02:01:35 +0100

    added logics for the isleaf function

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,6 +1,18 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_is_leaf - a function that checks if a node is a leaf
+ * @node: is a pointer to the node to check
+ * Return: 1 if node is a leaf, otherwise 0
+ */
 
 
-
-
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node == NULL)
+		return (0);
+	/* check if node has parent or has children */
+	if (node->parent != NULL || (node->left == NULL && node->right == NULL))
+		return (1);
+	return (0);
+}
