2fea0cdd1b4a517da38c9109571bdf5a0fd0dd34
Dennis Adigwe <adigwedennis@gmail.com>
2024-03-01 18:14:47 +0100

    added a helper function to check if the nodes has equal leaves

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,5 +1,9 @@
 #include "binary_trees.h"
 
+
+
+int is_perfect(const binary_tree_t *tree, size_t height, size_t level);
+
 /**
  * binary_tree_is_perfect - checks if a binary tree is perfect
  * @tree: pointer to a node
@@ -9,14 +13,9 @@
 
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	int r_height = 0, l_height = 0;
+	size_t height = binary_tree_height(tree);
 
-	if (tree == NULL)
-		return (1);
-	r_height = binary_tree_height(tree->right);
-	l_height = binary_tree_height(tree->left);
-	return ((r_height == l_height) && binary_tree_is_perfect(tree->left) &&
-			binary_tree_is_perfect(tree->right));
+	return (is_perfect(tree, height, 0));
 }
 
 /**
@@ -37,4 +36,29 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		return ((l_height > r_height) ? l_height : r_height);
 	}
 	return (0);
+}
+
+
+/**
+ * is_perfect - A helper function to check the child of a tree
+ * @tree: pointer to the node
+ * @height: the height of the node
+ * @level: the level or depth
+ * Return: 1 if equal otherwise 0
+ */
+
+
+int is_perfect(const binary_tree_t *tree, size_t height, size_t level)
+{
+	/** An empty tree is perfect */
+	if (tree == NULL)
+		return (1);
+
+	/** If a leaf node is reached, check if its level is equal to the height */
+	if (tree->left == NULL && tree->right == NULL)
+		return (level == height);
+
+	/** Recursively check left and right subtrees */
+	return (is_perfect(tree->left, height, level + 1) &&
+		is_perfect(tree->right, height, level + 1));
 }
