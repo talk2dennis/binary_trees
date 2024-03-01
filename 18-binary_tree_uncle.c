e91c15347085179943e31d6985c095fba4278e54
Dennis Adigwe <adigwedennis@gmail.com>
2024-03-01 17:45:48 +0100

    find the uncle of a node

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_uncle - finds the uncle of a node
+ * @node: is a pointer to the node to find the sibling
+ * Return: null if node or parent is null otherwise return uncle node
+ */
+
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	if (node == NULL || node->parent->parent == NULL)
+		return (NULL);
+	if (node->parent->parent->left == node->parent)
+		return (node->parent->parent->right);
+	return (node->parent->parent->left);
+}
